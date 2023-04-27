5d41a55fa418c6f377b71005be0b7b2a5c5307dc
张强 <zhangq@yxt.com>
2023-04-27 21:24:22 +0800

    去掉.history目录

--- vite.config.ts
+++ vite.config.ts
@@ -17,9 +17,8 @@ export default defineConfig({
       vueTemplate: true,
       dts: './types/auto-imports.d.ts',
       eslintrc: {
-        enabled: false, // 默认false, true启用。生成一次就可以，避免每次工程启动都生成，一旦生成配置文件之后，最好把enable关掉，即改成false
-        //否则这个文件每次会在重新加载的时候重新生成，这会导致eslint有时会找不到这个文件。当需要更新配置文件的时候，再重新打开
-        filepath: './.eslintrc-auto-import.json', // 生成json文件,可以不配置该项，默认就是将生成在根目录
+        enabled: false, // 默认false, true启用。生成一次就可以，避免每次工程启动都生成，当需要更新配置文件的时候，再重新打开
+        filepath: './.eslintrc-auto-import.json',
         globalsPropValue: true
       }
     }),
